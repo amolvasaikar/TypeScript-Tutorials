81c17b5569b8315336dbde53bde7424bdda745f6
Your Name <you@example.com>
2021-05-27 18:02:36 +0530

    Basic of TS
    Basic of TS

---
+++ TypeScriptModules/app.js
@@ -0,0 +1,6 @@
+"use strict";
+const cont = 10;
+console.log(cont);
+// Arrow Function
+const add = (num, num1) => { return num + num1; };
+console.log(add(10, 20));
---
+++ TypeScriptModules/app.ts
@@ -0,0 +1,11 @@
+
+const cont = 10;
+
+
+console.log(cont);
+
+// Arrow Function
+
+const add  = (num : number , num1 : number) => {  return num + num1 };
+
+console.log(add(10,20));
